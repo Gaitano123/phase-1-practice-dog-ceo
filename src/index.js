b91e25c085943409d51d8e095968c324df1ca98f
Gaitano <gaitano.george@student.moringaschool.com>
2023-07-07 17:04:53 +0300

    Comleted

--- src/index.js
+++ src/index.js
@@ -1,1 +1,66 @@
 console.log('%c HI', 'color: firebrick')
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+      .then(response => response.json()) 
+      .then(data => {
+        const dogImages = data.message; 
+
+
+        const dogImageContainer = document.getElementById("dog-image-container");
+
+        
+        dogImages.forEach(imageUrl => {
+          const imageElement = document.createElement("img");
+          imageElement.src = imageUrl; 
+
+
+          dogImageContainer.appendChild(imageElement);
+        });
+      })
+      .catch(error => {
+        console.log("Error fetching dog images:", error);
+      });
+
+
+    fetch("https://dog.ceo/api/breeds/list/all")
+      .then(response => response.json()) 
+      .then(data => {
+        const breeds = data.message; 
+
+
+        const dogBreedsContainer = document.getElementById("dog-breeds");
+
+        const listItems = [];
+
+        for (const breed in breeds) {
+          const listItemElement = document.createElement("li");
+          listItemElement.textContent = breed;
+
+
+          listItems.push(listItemElement);
+
+
+          dogBreedsContainer.appendChild(listItemElement);
+        }
+
+        const breedDropdown = document.getElementById("breed-dropdown");
+
+        breedDropdown.addEventListener("change", function() {
+          const selectedLetter = breedDropdown.value; // Get the selected letter from the dropdown
+
+          
+          listItems.forEach(listItem => {
+            if (listItem.textContent.startsWith(selectedLetter)) {
+              listItem.style.display = "list-item"; // Show the list item
+            } else {
+              listItem.style.display = "none"; 
+            }
+          });
+        });
+      })
+      .catch(error => {
+        console.log("Error fetching dog breeds:", error);
+      });
+  });
